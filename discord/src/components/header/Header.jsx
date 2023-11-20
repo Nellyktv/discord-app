29a112a041e1fb7e8297b9bf107f90da697504ce
Nellyktv <kitkaty965@gmail.com>
2023-11-20 16:01:20 +0300

    DISCORD-4: initialization react app

---
+++ discord/src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import style from './header.module.css';
+
+
+
+
+const Header = () => {
+
+    return (
+        <>
+
+
+        </>
+
+    );
+
+}
+
+export default Header;
